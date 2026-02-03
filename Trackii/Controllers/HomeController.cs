f90da73f23a152ecac132de0af830ef20ecbd749
hayfel <hayfeldev@gmail.com>
2026-02-02 19:54:53 -0700

    Expand gerencia views and fix WIP data handling

--- Trackii/Controllers/HomeController.cs
+++ Trackii/Controllers/HomeController.cs
@@ -8,6 +8,6 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        return RedirectToAction("Index", "Lobby");
+        return RedirectToAction("Index", "Gerencia");
     }
 }
